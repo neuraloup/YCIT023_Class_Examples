cfa52248d4b023b0beeed72f5dd1d9a91bf896ee
Niomi <2442040+neuraloup@users.noreply.github.com>
2022-09-21 00:10:40 -0400

    Update a1q1.js
    This script was not calculating the tip, updated "==" to "=" on to {tip_amount = GREAT_SERVICE; } etc on lines 17, 18, 19 and 20 and the script is now calculating tip on line 23 grandTotalIncTip.

--- Class9/a1q1.js
+++ Class9/a1q1.js
@@ -14,10 +14,10 @@ const GREAT_SERVICE = .20;
 const SERVICE_OBTAINED = "B";
 let tip_amount = 0.00;
 
-if (SERVICE_OBTAINED === "A") { tip_amount == GREAT_SERVICE; }
-if (SERVICE_OBTAINED === "B") { tip_amount == GOOD_SERVICE; }
-if (SERVICE_OBTAINED === "C") { tip_amount == OK_SERVICE; }
-if (SERVICE_OBTAINED === "D") { tip_amount == BAD_SERVICE; }
+if (SERVICE_OBTAINED === "A") { tip_amount = GREAT_SERVICE; }
+if (SERVICE_OBTAINED === "B") { tip_amount = GOOD_SERVICE; }
+if (SERVICE_OBTAINED === "C") { tip_amount = OK_SERVICE; }
+if (SERVICE_OBTAINED === "D") { tip_amount = BAD_SERVICE; }
 
 let totalPriceIncTax = (PRICE_OF_MEAL * (GST + PST)) + PRICE_OF_MEAL;
 let grandTotalIncTip = totalPriceIncTax * (1 + tip_amount);
